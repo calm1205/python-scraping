f5413d9d9db7411853bf69e0ac4e3ce3f9eb41d8
calm1205 <authentic.nagi@gmail.com>
2024-10-26 20:05:57 +0900

    feat: pip install beautifulsoup4

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
 ENDPOINT = 'https://dummyjson.com/test' # dummy data
 ENDPOINT = 'https://www.wantedly.com/projects?new=true&page=1&hiringTypes=internship&hiringTypes=part_time&order=mixed'
 
+HREF_CLASS = 'ProjectListJobPostItem__CompanyLink-sc-bjcnhh-10 oYEY'
+
 with urlopen(ENDPOINT) as response:
+    soup = BeautifulSoup(html, "html.parser")
+    title = soup.find("h1").text
+    print(title)
     for line in response:
         line = line.decode('utf-8')
-        print(line)
+        if HREF_CLASS in line:
+            print('hit')
