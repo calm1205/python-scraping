d674f9fe051bddea73435a1e57d5e1f468d5148d
calm1205 <authentic.nagi@gmail.com>
2024-10-26 20:39:34 +0900

    feat: page_increment

---
+++ src/page_increment.py
@@ -0,0 +1,29 @@
+from urllib.parse import urlparse, parse_qs, urlunparse, urlencode
+from main import ENDPOINT
+
+
+def page_increment(*, url):
+    """
+    urlのpageをインクリメント
+    """
+
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+
+    current_page = query_params.get("page")[0]
+
+    query_params["page"] = int(current_page) + 1
+
+    new_query_string = urlencode(query_params, doseq=True)
+    new_url = urlunparse(
+        (
+            parsed_url.scheme,
+            parsed_url.netloc,
+            parsed_url.path,
+            "",
+            new_query_string,
+            "",
+        )
+    )
+
+    return new_url
